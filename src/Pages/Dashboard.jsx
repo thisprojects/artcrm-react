b756df9d693cc7b0d2a8311d1a10dccb5a2f0256
Nathan Downes <p2677720@my365.dmu.ac.uk>
2022-08-16 13:53:18 +0100

    update dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -17,41 +17,56 @@ const Dashboard = () => {
     setResponse(response);
   };
 
-  const options = {
-    title: {
-      text: "My chart",
-    },
-    series: [
-      {
-        data: [1, 2, 3],
-      },
-    ],
-  };
-
   useEffect(() => {
     getAnalytics();
   }, []);
 
   const eventData = response && response.mostRecentEvents;
-  const contactData = response && response.lastFiveContactSignups;
+
+  const allPostCodes = response && response.postcodes;
 
   const eventCatagories = eventData?.map((item) => item.name);
   const eventSeries = eventData?.map((item) => item.contactCount);
 
-  const postCodeStats = contactData?.reduce((acc, curr) => {
-    if (!acc[curr.postCode]) {
-      acc[curr.postCode] = 1;
+  const ageDemographics = {
+    "Under 10": 0,
+    "Ten to Twenty": 0,
+    "Twenty to Forty": 0,
+    "Forty to Sixty": 0,
+    "Sixty Plus": 0,
+  };
+
+  response?.ageDemographic?.forEach((item) => {
+    if (item.age === null) return;
+    if (item.age < 10) ageDemographics["Under 10"] += 1;
+    if (item.age >= 10 && item.age <= 20) ageDemographics["Ten to Twenty"] += 1;
+    if (item.age > 20 && item.age <= 40)
+      ageDemographics["Twenty to Forty"] += 1;
+    if (item.age > 40 && item.age <= 60) ageDemographics["Forty to Sixty"] += 1;
+    if (item.age > 60) ageDemographics["Sixty Plus"] += 1;
+  });
+
+  console.log("DEMOS", ageDemographics);
+  const ageDemographicCatagories =
+    (ageDemographics && Object.keys(ageDemographics)) || [];
+  const ageDemographicSeries =
+    (ageDemographics && Object.values(ageDemographics)) || [];
+
+  const allPostCodeStats = allPostCodes?.reduce((acc, curr) => {
+    if (!acc[curr]) {
+      acc[curr] = 1;
     } else {
-      acc[curr.postCode]++;
+      acc[curr]++;
     }
     return acc;
   }, {});
 
-  console.log("post code", postCodeStats);
+  console.log("post code", allPostCodeStats);
 
   const postCodeCatagories =
-    (postCodeStats && Object.keys(postCodeStats)) || [];
-  const postCodeSeries = (postCodeStats && Object.values(postCodeStats)) || [];
+    (allPostCodeStats && Object.keys(allPostCodeStats)) || [];
+  const postCodeSeries =
+    (allPostCodeStats && Object.values(allPostCodeStats)) || [];
 
   // const postCodes = contactData?.map((item) => item.postCodes);
 
@@ -64,28 +79,44 @@ const Dashboard = () => {
       <Box sx={{ padding: "10px" }}>
         <h1>Dashboard</h1>
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          <Grid item md={3}>
-            Number of Contacts
+          <Grid item md={4}>
+            <p>Total Contacts: {response?.numberOfContacts} </p>
           </Grid>
-          <Grid item md={3}>
-            Number of Organisations
+          <Grid item md={4}>
+            <p>Total Organisations: {response?.numberOfOrganisations} </p>
           </Grid>
-          <Grid item md={3}>
-            Number of Events
+          <Grid item md={4}>
+            <p>Total Events: {response?.numberOfEvents}</p>
           </Grid>
-          <Grid item md={3}>
-            Most recent contacts
-            {contactData?.map((item) => (
-              <p>
-                {item.firstName} {item.lastName}
-              </p>
-            ))}
-          </Grid>
-          <Grid item md={12}>
-            Most Recent Events
+          <Grid item md={6}>
             <HighchartsReact
               highcharts={Highcharts}
               options={{
+                colors: ["#2f7ed8", "#0d233a", "#8bbc21", "#910000", "#1aadce"],
+                plotOptions: {
+                  column: {
+                    colorByPoint: true,
+                  },
+                },
+                chart: {
+                  type: "column",
+                },
+                xAxis: {
+                  categories: ageDemographicCatagories,
+                  crosshair: true,
+                },
+                title: {
+                  text: "Age Demographics",
+                },
+                series: [{ data: ageDemographicSeries }],
+              }}
+            />
+          </Grid>
+          <Grid item md={6}>
+            <HighchartsReact
+              highcharts={Highcharts}
+              options={{
+                colors: ["#492970", "#f28f43", "#77a1e5", "#c42525", "#a6c96a"],
                 plotOptions: {
                   column: {
                     colorByPoint: true,
@@ -97,31 +128,20 @@ const Dashboard = () => {
                 xAxis: {
                   categories: eventCatagories,
                   crosshair: true,
-                  colors: [
-                    "#2f7ed8",
-                    "#0d233a",
-                    "#8bbc21",
-                    "#910000",
-                    "#1aadce",
-                    "#492970",
-                    "#f28f43",
-                    "#77a1e5",
-                    "#c42525",
-                    "#a6c96a",
-                  ],
                 },
                 title: {
-                  text: "chart",
+                  text: "Five Most Recent Events",
                 },
                 series: [{ data: eventSeries }],
               }}
             />
           </Grid>
           <Grid item md={12}>
-            Post Code Stats
-            {postCodeStats && (
+            {allPostCodeStats && (
               <HighchartsReact
                 highcharts={Highcharts}
+                umber
+                of
                 options={{
                   plotOptions: {
                     column: {
@@ -148,7 +168,7 @@ const Dashboard = () => {
                     ],
                   },
                   title: {
-                    text: "chart",
+                    text: "Postcode Demographic - All Contacts",
                   },
                   series: [{ data: postCodeSeries }],
                 }}
